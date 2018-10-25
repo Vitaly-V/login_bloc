076f62fc107b246e974883370541deb72478d42b
Vitaly-V <gtopbox@gmail.com>
2018-10-25 16:52:49 +0300

    Init

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+        ],
+      )
+    );
+  }
+
+  Widget emailField() {
+
+  }
+
+  Widget passwordField() {
+
+  }
+}
